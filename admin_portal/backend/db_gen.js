9fa88671531870706248c4fff533d870c7f9ebb3
“lakshmikant” <fullstacktrainer4u@gmail.com>
2024-04-25 21:26:47 +0530

    Actor movie many to many table created

--- admin_portal/backend/db_gen.js
+++ admin_portal/backend/db_gen.js
@@ -45,9 +45,17 @@ async function createMoviesTable() {
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
         `
+        const movie_actor = `CREATE TABLE IF NOT EXISTS movie_actors (
+            movie_id INT,
+            actor_id INT,
+            PRIMARY KEY (movie_id, actor_id),
+            FOREIGN KEY (movie_id) REFERENCES movies(id),
+            FOREIGN KEY (actor_id) REFERENCES actors(id)
+        );
+        `
         // Execute the SQL query
-        await client.query(actor_query);
-        console.log('Actor table created successfully');
+        await client.query(movie_actor);
+        console.log('Actor <> Movie table created successfully');
     } catch (error) {
         console.error('Error creating movies table:', error);
     } finally {
