20c0d4c6dd88370b4db977ca20152cff18bfc7a8
“lakshmikant” <fullstacktrainer4u@gmail.com>
2024-04-26 02:01:25 +0530

    Added Sidenav

--- admin_portal/frontend/src/App.js
+++ admin_portal/frontend/src/App.js
@@ -1,12 +1,15 @@
 // App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Sidenav from './components/Sidenav'; // Import the Sidenav component
 import Movies from './components/Movies';
 import './App.css';
+
 const App = () => {
   return (
     <Router>
       <div className='flex'>
+        <Sidenav />
         <Routes>
           <Route path="/movies" element={<Movies />} />
           <Route path="/" element={<Navigate to="/movies" />} />
---
+++ admin_portal/frontend/src/components/Sidenav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidenav = () => {
+    return (
+        <div className={`fixed inset-y-0 left-0 w-64 bg-gray-800 text-white transition duration-300 ease-in-out transform lg:translate-x-0 lg:block hidden`}>
+            <div className="flex justify-between items-center p-4">
+                <h1 className="text-2xl font-bold">IMDB</h1>
+            </div>
+            <div className="mt-4">
+                <Link to="/movies" className="block px-4 py-2 text-lg text-white hover:bg-gray-700">Movies</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Sidenav;
