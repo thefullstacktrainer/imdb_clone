c16288cc3bd253d4def47bab87bed99b83bf633b
“lakshmikant” <fullstacktrainer4u@gmail.com>
2024-05-02 04:04:48 +0530

    unit testing

--- admin_portal/backend/test/mockDb.js
+++ admin_portal/backend/test/mockDb.js
@@ -1,14 +1,9 @@
-// mockDb.js
-
 const getClient = () => {
     // Mock implementation of the database client
     return {
-        query: jest.fn().mockResolvedValue({
-            rows: [{ id: 1, title: 'Test Movie', description: 'Test Description', genre: 'Test Genre', poster_url: 'https://example.com/test.jpg' }]
-        })
+        query: jest.fn(),
     };
 };
-
 
 const connect = jest.fn(); // Mock the connect function
 
--- admin_portal/backend/test/movies.test.js
+++ admin_portal/backend/test/movies.test.js
@@ -8,16 +8,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api/movies', router);
 var client;
+
 const getClient2 = () => {
     client = getClient();
     return client;
-}
+};
+
 // Mock the database module
 jest.mock('../src/db', () => {
     const { connect } = require('./mockDb');
 
     return {
-        getClient: getClient2, connect
+        getClient: getClient2,
+        connect,
     };
 });
 
@@ -31,13 +34,12 @@ describe('POST /api/movies', () => {
             poster_url: 'https://example.com/test.jpg',
         };
 
+        jest.spyOn(client, 'query').mockResolvedValueOnce({ rows: [newMovie] });
         const response = await request(app)
             .post('/api/movies')
             .send(newMovie);
 
-        // Assert that the response status code is 201 (Created)
         expect(response.statusCode).toBe(201);
-        // Assert that the response body contains the inserted movie details
         expect(response.body.success).toBe(true);
         expect(response.body.movie.title).toBe(newMovie.title);
         expect(response.body.movie.description).toBe(newMovie.description);
@@ -47,16 +49,12 @@ describe('POST /api/movies', () => {
 });
 
 describe('GET /api/movies', () => {
-    // Get the mocked client object
-
-
     it('should fetch all movies', async () => {
         const mockMovies = [
             { id: 1, title: 'Movie 1', description: 'Description 1', release_date: '2023-01-01', genre: 'Action', poster_url: 'https://example.com/movie1.jpg' },
             { id: 2, title: 'Movie 2', description: 'Description 2', release_date: '2024-01-01', genre: 'Comedy', poster_url: 'https://example.com/movie2.jpg' }
         ];
 
-        // Mock the query method of the client object to return mock movies
         jest.spyOn(client, 'query').mockResolvedValueOnce({ rows: mockMovies });
         const response = await request(app).get('/api/movies');
 
@@ -66,7 +64,6 @@ describe('GET /api/movies', () => {
     });
 
     it('should handle errors when fetching movies', async () => {
-        // Mock the query method of the client object to throw an error
         jest.spyOn(client, 'query').mockRejectedValueOnce(new Error('Database error'));
 
         const response = await request(app).get('/api/movies');
@@ -109,6 +106,110 @@ describe('GET /api/movies/:id', () => {
         jest.spyOn(client, 'query').mockRejectedValueOnce(new Error('Database error'));
 
         const response = await request(app).get(`/api/movies/${movieId}`);
+
+        expect(response.statusCode).toBe(500);
+        expect(response.body.success).toBe(false);
+        expect(response.body.error).toBe('Internal Server Error');
+    });
+});
+
+describe('PUT /api/movies/:movieId/actors', () => {
+
+    it('should handle error when movie not found', async () => {
+        const movieId = 999;
+        const addActorIds = [1, 2]; // Mock actor IDs to add
+        const removeActorIds = [3, 4]; // Mock actor IDs to remove
+
+        // Mock the query to check if the movie exists
+        jest.spyOn(client, 'query').mockResolvedValueOnce({ rows: [] });
+
+        const response = await request(app)
+            .put(`/api/movies/${movieId}/actors`)
+            .send({ addActorIds, removeActorIds });
+
+        expect(response.statusCode).toBe(404);
+        expect(response.body.success).toBe(false);
+        expect(response.body.error).toBe('Movie not found');
+    });
+
+    it('should handle error when one or more actors not found', async () => {
+        const movieId = 1;
+        const addActorIds = [1, 2, 3]; // Mock actor IDs to add
+        const removeActorIds = [4, 5]; // Mock actor IDs to remove
+
+        // Mock the query to check if the movie exists
+        jest.spyOn(client, 'query').mockResolvedValueOnce({ rows: [{ id: movieId }] });
+
+        // Mock the query to check if the actors to be added exist
+        jest.spyOn(client, 'query').mockResolvedValueOnce({ rows: [{ id: 1 }, { id: 2 }] });
+
+        // Mock the query to check if the actors to be removed exist
+        jest.spyOn(client, 'query').mockResolvedValueOnce({ rows: [{ id: 4 }] });
+
+        const response = await request(app)
+            .put(`/api/movies/${movieId}/actors`)
+            .send({ addActorIds, removeActorIds });
+
+        expect(response.statusCode).toBe(404);
+        expect(response.body.success).toBe(false);
+        expect(response.body.error).toBe('One or more actors not found');
+    });
+
+    it('should handle internal server error', async () => {
+        const movieId = 1;
+        const addActorIds = [1, 2]; // Mock actor IDs to add
+        const removeActorIds = [3, 4]; // Mock actor IDs to remove
+
+        // Mock the query to check if the movie exists
+        jest.spyOn(client, 'query').mockRejectedValueOnce(new Error('Database error'));
+
+        const response = await request(app)
+            .put(`/api/movies/${movieId}/actors`)
+            .send({ addActorIds, removeActorIds });
+
+        expect(response.statusCode).toBe(500);
+        expect(response.body.success).toBe(false);
+        expect(response.body.error).toBe('Internal Server Error');
+    });
+
+});
+
+
+describe('DELETE /api/movies/:id', () => {
+
+    it('should delete a movie by ID', async () => {
+        const movieId = 1;
+
+        // Mock the first query to check if the movie exists
+        jest.spyOn(client, 'query').mockResolvedValueOnce({ rows: [{ id: movieId }] });
+
+        // Mock the second query to delete the movie
+        jest.spyOn(client, 'query').mockResolvedValueOnce();
+        const response = await request(app).delete(`/api/movies/${movieId}`);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.success).toBe(true);
+        expect(response.body.message).toBe('Movie deleted successfully');
+    });
+
+    it('should handle error when movie not found', async () => {
+        const movieId = 999;
+
+        jest.spyOn(client, 'query').mockResolvedValueOnce({ rows: [] });
+
+        const response = await request(app).delete(`/api/movies/${movieId}`);
+
+        expect(response.statusCode).toBe(404);
+        expect(response.body.success).toBe(false);
+        expect(response.body.error).toBe('Movie not found');
+    });
+
+    it('should handle errors when deleting movie by ID', async () => {
+        const movieId = 1;
+
+        jest.spyOn(client, 'query').mockRejectedValueOnce(new Error('Database error'));
+
+        const response = await request(app).delete(`/api/movies/${movieId}`);
 
         expect(response.statusCode).toBe(500);
         expect(response.body.success).toBe(false);
